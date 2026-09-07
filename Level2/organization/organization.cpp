c93d16e03c536323628fe3f62067109f2d4b2c64
Arego11 <aregar20012001@gmail.com>
2026-09-07 13:03:48 -0700

    minor changes

--- Level2/organization/organization.cpp
+++ Level2/organization/organization.cpp
@@ -14,37 +14,39 @@ class Company {
         // you will accept the workers' vector,
         // will iterate through them
         // store the seen workers in a hashset 
+        std::unordered_set<int> pipeline;
         std::unordered_set<int> seen;
         int rootCounter = 0;
         // a for loop to iterate through the workers
         for (int i = 0; i < workers.size(); i++){
             for (int employeeID : workers[i].directReports){
                 if (seen.contains(employeeID)) return false;
+                pipeline.insert(i);
                 seen.insert(employeeID);
             }
         }
         for (int i = 0 ; i < workers.size(); i++){
-            if (!seen.contains(workers[i].id)) rootCounter++;
+            if (!seen.contains(workers[i].id) || !pipeline.contains(workers[i].id)) rootCounter++;
         }
         if (rootCounter != 1) return false;
-        
+
         return true;
     }
 };
 
 
 int main () {
+    std::vector<Employees> workers = {
+        {1, {2, 3}},
+        {2, {4, 58}},
+        {3, {6}},
+        {4, {}},
+        {5, {}},
+        {6, {}}
+    };
+    Company company;
 
+    std::cout << company.checker(workers) << std::endl;
+
+    return 0;
 }
-
-
-
-
-
-// @new members. Hello everyone, welcome to the team. 
-// Everyone on the software team, please put your information in here, 
-// This will be used to track our progress over the semester. 
-// Once we finish our 2026 comp, I will have more time to set
-// up our meetings and we will see who works on what.
-// There are TONS of things to work on.
-
