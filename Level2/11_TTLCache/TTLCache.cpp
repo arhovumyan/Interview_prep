413bb98ea50dded92834b00c87277c3916c176b3
Arego11 <aregar20012001@gmail.com>
2026-09-10 23:57:31 -0700

    ttl data handler

---
+++ Level2/11_TTLCache/TTLCache.cpp
@@ -0,0 +1,52 @@
+// REQUIREMENTS
+// build a key value cache where every stored item has an expiration time
+// need a put(key, value, expiration_time) and get(key, now)
+// MAIN RULES AND RESTRICTIONS
+// must never return an expired data
+// THOUGHT PROCESS
+// we need to figure out how to store all these values.
+// we can store key and value in a hashmap, but we need a third value, expiration date,
+// im thinking of a Struct that would contain all these data like this
+#include <vector>
+#include <iostream>
+
+struct Data {
+    int key;
+    int value;
+    int expires_at;
+};
+
+std::vector<Data> storage;
+
+class DataHandle {
+    public:
+    void put(const Data& item) {
+        storage.push_back(item);
+    }
+
+    int get(int key, int now){
+        for (const Data& x : storage){
+            if (key == x.key) {
+                if (now >= x.expires_at) return -1;
+            }
+            return x.value;
+        }
+        return -1;
+    }
+};
+
+int main () {
+    
+    std::vector<Data> items = {
+    {1, 131232, 3},
+    {2, 54321, 5},
+    {3, 99999, 8},
+    {4, 12345, 10}
+    };
+    
+    DataHandle handler; // create an object named handler
+    handler.put(item1);
+    std::cout << handler.get(1, 2) << std::endl;
+
+    return 0;
+}
